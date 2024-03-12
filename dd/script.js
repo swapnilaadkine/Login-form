7f8cdeff3f30df19562fe62a046452a929293e96
swapniladv9@gmail.com <swapniladv9@gmail.com>
2024-03-12 19:01:46 +0530

    login form

---
+++ dd/script.js
@@ -0,0 +1,30 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconclose = document.querySelector('.icon-close');
+
+
+
+
+
+
+registerLink.addEventListener('click', ()=> {
+  wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+   wrapper.classList.remove('active');
+});
+btnPopup.addEventListener('click',() => {
+wrapper.classList.add('active-popup');
+});
+iconclose.addEventListener('click', () => {
+     wrapper.classList.remove('active-popup');
+});
+
+
+
+function hello(){
+    alert("gandu he sgl kelya n khi nhi hot abhyasavr lksh de fkt");
+}
